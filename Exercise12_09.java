e78347259f33adeb59bb9f866372e0ad08a1fe98
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-06-22 14:52:19 -0400

    convert binary string to decimal, convert binary to decimal

--- Exercise12_09.java
+++ Exercise12_09.java
@@ -1,5 +1,43 @@
 
 public class Exercise12_09 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a binary string: ");
+        String binaryString = input.next();
+
+        //try to convert and display
+        try {
+            System.out.println(binaryString + " in decimal is " + bin2Dec(binaryString));
+        } 
+        catch (BinaryFormatException ex) {
+        // display custom message
+        System.out.println(ex.getMessage());
+        }
+
+        input.close();
     }
 }
+/** Convert binary string to decimal */
+ public static int bin2Dec(String binaryString) throws BinaryFormatException {
+
+    for (int i = 0; i < binaryString.length(); i++) {
+        if (binaryString.charAt(i) != '0' 
+        && binaryString.charAt(i) != '1') {
+            throw new BinaryFormatException(
+                binaryString + " is not a binary string");
+
+        }
+    }
+
+    //convert binary to decimal
+    int decimalValue = 0;
+    for (int i = 0; i < binaryString.length(); i++) {
+        decimalValue = decimalValue * 2 + 
+        (binaryString.charAt(i) - '0');
+    }
+
+    // return
+    return decimalValue;
+
+ }
